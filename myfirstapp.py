92f118246f4c17c79d03759ccd879f6dadc248a0
syamil003 <156885172+syamil003@users.noreply.github.com>
2024-01-17 15:30:07 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -27,10 +27,8 @@ elif option=='T n C':
         }))
 elif option=='Long Process':
     'Starting a long computation...'
-
     latest_iteration = st.empty()
     bar = st.progress(0)
-
     for i in range(100):
         latest_iteration.text(f'Iteration {i+1}')
         bar.progress(i + 1)
