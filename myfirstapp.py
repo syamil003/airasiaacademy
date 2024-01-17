856d7be2e1f2f4403084176122b135c03fc21aed
syamil003 <156885172+syamil003@users.noreply.github.com>
2024-01-17 15:31:50 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -24,18 +24,17 @@ elif option=='T n C':
         st.write(pd.DataFrame({
             'Students': ['John', 'Lofa', 'Siti', 'Amy'],
             'Attendance Status': ['yes', 'yes', 'yes', 'no']
-        }))
 elif option=='Long Process':
     'Starting a long computation...'
+
     latest_iteration = st.empty()
     bar = st.progress(0)
+
     for i in range(100):
         latest_iteration.text(f'Iteration {i+1}')
         bar.progress(i + 1)
         time.sleep(0.1)
 
     '...and now we\'re done!'
-
-
-
-
+        }))
+        
