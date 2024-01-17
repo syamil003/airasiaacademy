99e74972fbb071e470bbdb58a201fbd7d7471f1a
syamil003 <156885172+syamil003@users.noreply.github.com>
2024-01-17 15:24:04 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -17,12 +17,6 @@ elif option=='map':
     columns=['lat', 'lon'])
     st.map(map_data) 
 elif option=='T n C':
-    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-    show = st.checkbox('I agree the terms and conditions')
-    if show:
-        st.write(pd.DataFrame({
-        'Students': ['John', 'Lofa', 'Siti', 'Amy'],
-        'Attendance Status': ['yes', 'yes', 'yes', 'no']
-        }))
+    st.write("test")
 
 
